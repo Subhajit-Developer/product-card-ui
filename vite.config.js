bf25e3f25d78e44026d2deb23da91226d79779d7
Subhajit Gorain <110682313+Subhajit-Developer@users.noreply.github.com>
2025-07-25 03:05:04 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,4 @@
 // vite.config.js
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  base: '/product-card-ui/', // 👈 Must match your repo name
-  plugins: [react()],
-})
+export default {
+  base: "/product-card-ui/", // <-- this is important!
+};
